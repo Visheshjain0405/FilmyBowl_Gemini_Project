141b75bda455266658d0d15283bf435ad7d9a792
Vishesh Jain <Visheshjain0405@gmail.com>
2025-10-28 01:15:22 +0530

    Update axios.js

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:9000/api',
+  baseURL: 'https://filmybowl-gemini-project.onrender.com/api',
   timeout: 60000,
 });
 
